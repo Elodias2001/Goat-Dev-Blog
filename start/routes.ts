2e894049b4033153e04a65767267ce579e386018
Elodias2001 <olouwagnon@gmail.com>
2024-07-29 16:51:25 +0100

    TestConnexion

--- start/routes.ts
+++ start/routes.ts
@@ -7,10 +7,13 @@
 |
 */
 
+const HomeController = () => import('#controllers/home_controller')
 import router from '@adonisjs/core/services/router'
 const AuthController = () => import('#controllers/Auth/auth_controller')
 
-router.on('/').render('pages/home')
+// router.on('/').render('pages/home')
+
+router.get('/', [HomeController, 'home']).as('home')
 
 router.get('/register', [AuthController, 'register']).as('auth.register')
 router.post('/register', [AuthController, 'handleRegister']).as('handleRegister')
