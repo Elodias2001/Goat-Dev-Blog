85f33eb382fa45238458dacf6fd7ac0c161310c4
Elodias2001 <olouwagnon@gmail.com>
2024-07-29 18:46:22 +0100

    SocialLinksInsertion

--- start/routes.ts
+++ start/routes.ts
@@ -10,14 +10,18 @@
 import router from '@adonisjs/core/services/router'
 const HomeController = () => import('#controllers/home_controller')
 const AuthController = () => import('#controllers/Auth/auth_controller')
+import { middleware } from '#start/kernel'
 
 // router.on('/').render('pages/home')
-
 router.get('/', [HomeController, 'home']).as('home')
 
-router.get('/register', [AuthController, 'register']).as('auth.register')
-router.post('/register', [AuthController, 'handleRegister']).as('handleRegister')
+router
+  .group(() => {
+    router.get('/register', [AuthController, 'register']).as('auth.register')
+    router.post('/register', [AuthController, 'handleRegister']).as('handleRegister')
 
-router.get('/login', [AuthController, 'login']).as('auth.login')
-router.post('/login', [AuthController, 'handleLogin']).as('handleLogin')
-router.delete('/login', [AuthController, 'logout']).as('auth.logout')
+    router.get('/login', [AuthController, 'login']).as('auth.login')
+    router.post('/login', [AuthController, 'handleLogin']).as('handleLogin')
+  })
+  .use(middleware.guest())
+router.delete('/login', [AuthController, 'logout']).as('auth.logout').use(middleware.auth())
